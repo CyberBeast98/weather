ab77371dbade7e48ac67592d44e270cd457c8a33
CyberBeast98 <artemkamenskyi1998@gmail.com>
2023-08-09 17:42:33 +0300

    added scroll to top button component

---
+++ src/components/scroll-button.vue
@@ -0,0 +1,27 @@
+<template>
+  <button @click="$emit('scrollToTop')">
+    <img src="../assets/icons/down.svg" alt="down-icon">
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'ScrollButton'
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+  position: fixed;
+  bottom: 50px;
+  right: 50px;
+  background: transparent;
+  border-radius: 50%;
+  border: none;
+  transform: rotate(180deg);
+  img {
+    width: 50px;
+    height: 50px;
+  }
+}
+</style>
